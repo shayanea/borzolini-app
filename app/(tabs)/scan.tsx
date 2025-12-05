6229035ce15c59d4994ff25a3383358ad3c64205
Shayan Araghi <shayanaraghi@live.com>
2025-12-05 18:10:43 +0330

    feat: update scan tab and dependencies

--- app/(tabs)/scan.tsx
+++ app/(tabs)/scan.tsx
@@ -1,9 +1,11 @@
+
+import { ScanDashboard } from '@/features/scan/components';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function ScanScreen() {
   return (
     <SafeAreaView className="flex-1 bg-[#17171c]" edges={['top']}>
-      {/* <ScanDashboard /> */}
+      <ScanDashboard />
     </SafeAreaView>
   );
 }
